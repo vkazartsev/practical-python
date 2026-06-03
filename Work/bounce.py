eada4d085902394ea441084da709b1a754448426
Vladislav Kazartsev <vladislav.kazartsev@gmail.com>
2026-06-03 13:16:04 +0300

    exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+# Rubber ball dropped from 100 m.
+# Each time it hits the ground, it bounces back up to 3/5 the height it fell.
+# Print a table showing the height of the first 10 bounces.
+
+rebound_percentage = 0.6
+height = 100
+bounce = 1
+
+while bounce <= 10:
+    height = height * rebound_percentage
+    print(bounce, round(height,4))
+    bounce = bounce + 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,24 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+total_paid = 0.0
+current_month = 1
+
+while principal > 0:
+    monthly_payment = payment + extra_payment * ((current_month >= extra_payment_start_month) and (current_month <= extra_payment_end_month))
+    principal = principal * (1+rate/12)
+    monthly_payment = min(monthly_payment, principal)
+    principal = principal - monthly_payment
+    total_paid = total_paid + monthly_payment
+    current_month += 1
+    print(f'{current_month}, {total_paid}, {principal}')
+
+print(f'Total paid: {total_paid}')
